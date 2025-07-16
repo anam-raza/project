1b78442e6838a95390639f6896f391a6e8d5b111
anam-raza <anamraza521@gmail.com>
2025-07-16 12:26:26 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,19 @@
+mytuple =("anam","soumya","anam","anam",12 , True)
+
+#indexing
+print(mytuple[2])
+print(mytuple[-2])
+
+
+# #slicing
+# print(mytuple[1:2])
+
+# #count method
+# print(mytuple.count("anam"))
+
+
+# print(mytuple.index("anam"))
+
+
+# mylist = (mytuple)
+# print(mylist)
